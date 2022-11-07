c64ded852d422bf5db1b87dd668971e65777469f
Ashraf59 <ashrafams59b@gmail.com>
2022-11-07 23:01:31 +0600

    home-food website

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCr9ik16PoTOSCP00f8R5BJPCEfMyHsdLg",
+  authDomain: "home-food-652e5.firebaseapp.com",
+  projectId: "home-food-652e5",
+  storageBucket: "home-food-652e5.appspot.com",
+  messagingSenderId: "96422897041",
+  appId: "1:96422897041:web:74e8855dc2d39604926630"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
