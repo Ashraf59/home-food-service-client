9498fa6b72e56b8a180b8adc1dd81c7fe4170486
Ashraf59 <ashrafams59b@gmail.com>
2022-11-08 16:48:29 +0600

    Home Banner and two section design

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import hero from '../../../Assets/hero/hero.jpg'
+import hero2 from '../../../Assets/hero/hero2.jpg';
 
 const Home = () => {
     return (
         //Banner Section
        <div>
 
-<div className="hero min-h-screen" style={{ backgroundImage: `url("https://placeimg.com/1000/800/arch")` }}>
+<div className="hero min-h-screen" style={{ backgroundImage: `url("https://domf5oio6qrcr.cloudfront.net/medialibrary/11537/4a78f148-d427-4209-8173-f33d04c44106.jpg")` }}>
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-center text-neutral-content">
     <div className="max-w-md">
@@ -26,8 +27,7 @@ const Home = () => {
                     <p className="mt-6 mb-8 text-lg sm:mb-12 text-slate-500">Homemade food is usually prepared with natural ingredients compared to commercially prepared food. Plainly, the food has its taste however those are ready using unprocessed foods that are by no means healthy.
                     </p>
                     <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
-                        <Link to='' className="px-8 py-3 text-lg font-semibold rounded bg-violet-400 dark:text-gray-900">Suspendisse</Link>
-                        <Link to='' className="px-8 py-3 text-lg font-semibold border rounded border-gray-100">Malesuada</Link>
+                    <Link to=''> <button className="btn btn-error text-white">Order Now</button></Link>
                     </div>
                 </div>
                 <div className=" p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
@@ -35,6 +35,25 @@ const Home = () => {
                 </div>
             </div>
         </section>
+
+
+        <section className="dark:bg-gray-800 dark:text-gray-100">
+	<div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+		<div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+        <img src={hero2} alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128 rounded shadow p-6" />
+		</div>
+		<div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+        <h1 className="text-5xl font-bold leading-none sm:text-6xl">Delicious Winter Pitha
+                    </h1>
+                    <p className="mt-6 mb-8 text-lg sm:mb-12 text-slate-500">The word “pitha” in Bangla refers to an indigenous food item that is nationally popular. Sweet dishes like bhapa pitha, patishapta, dudher shondesh, etc are the most common examples. “Utshob”, on the other hand, means festival. Thus, combined, pitha utshob is a glorious exhibition in Bangladesh during the winter season where people get together to set up stalls and sell their homemade pithas.
+                    </p>
+			<div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+				<Link to=''> <button className="btn btn-error text-white">Order Now</button></Link>
+				
+			</div>
+		</div>
+	</div>
+</section>
 
        </div>
 
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import image from '../../../Assets/images/logo.jpg';
 
 const Footer = () => {
     return (
         <footer className="footer p-8 bg-rose-500 text-white m-auto">
   <div className='text-center m-auto'>
+  <Link to='/'className="btn uppercase text-2xl font-bold"><span className='text-rose-500'>Home</span> Food</Link>
 <img src={image} alt="" />
   </div> 
   <div>
